d5a6b0d2eeadb273b0f95baf8634335948a973d7
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-09-17 21:07:55 +0200

    Create User Model

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/models/User.java
@@ -0,0 +1,74 @@
+package com.udacity.jwdnd.course1.cloudstorage.models;
+
+/*
+*
+*   userid INT PRIMARY KEY auto_increment,
+  username VARCHAR(20) UNIQUE,
+  salt VARCHAR,
+  password VARCHAR,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20)
+*
+* */
+public class User {
+
+    private Integer id;
+    private String userName;
+    private String password;
+    private String salt;
+    private String firstName;
+    private String lastName;
+
+    public User(String userName, String password, String salt, String firstName, String lastName) {
+        this.userName = userName;
+        this.password = password;
+        this.salt = salt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthService.java
@@ -1,10 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class AuthService implements AuthenticationProvider {
