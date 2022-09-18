506b780bbd0107064a0fb65a5bdaf2c50a4d7759
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-09-18 16:42:12 +0200

    add thymleaf attributes to login page

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthService;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private AuthService authenticationService;
+    private final AuthService authenticationService;
 
     public SecurityConfig(AuthService authenticationService) {
         this.authenticationService = authenticationService;
